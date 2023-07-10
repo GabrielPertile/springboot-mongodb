cd940307858d38e4ba54ab9479bb004e4c4a67b4
GabrielPertile <gabriel-pertile@hotmail.com>
2023-07-10 16:31:04 -0300

    Inserção de usuário com POST

--- src/main/java/com/gpertile/springbootmongo/resources/UserResource.java
+++ src/main/java/com/gpertile/springbootmongo/resources/UserResource.java
@@ -1,13 +1,17 @@
 package com.gpertile.springbootmongo.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.gpertile.springbootmongo.domain.User;
 import com.gpertile.springbootmongo.dto.UserDTO;
@@ -19,19 +23,31 @@ public class UserResource {
 
 	@Autowired
 	private UserService service;
-	
+
 	@GetMapping
 	public ResponseEntity<List<UserDTO>> findAll() {
 		List<User> list = service.findAll();
-		List<UserDTO> listDto = list.stream()
-				.map(x -> new UserDTO(x)).toList();
+		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).toList();
 		return ResponseEntity.ok().body(listDto);
 	}
-	
+
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
 		User obj = service.findById(id);
-		
+
 		return ResponseEntity.ok().body(new UserDTO(obj));
+	}
+
+	@PostMapping
+	public ResponseEntity<Void> insert(@RequestBody UserDTO objDto) {
+		User obj = service.fromDTO(objDto);
+		
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder
+				.fromCurrentRequest()
+				.path("/{id}")
+				.buildAndExpand(obj.getId())
+				.toUri();	
+		return ResponseEntity.created(uri).build();
 	}
 }
--- src/main/java/com/gpertile/springbootmongo/services/UserService.java
+++ src/main/java/com/gpertile/springbootmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.gpertile.springbootmongo.domain.User;
+import com.gpertile.springbootmongo.dto.UserDTO;
 import com.gpertile.springbootmongo.repository.UserRepository;
 import com.gpertile.springbootmongo.services.exception.ObjectNotFoundException;
 
@@ -24,5 +25,13 @@ public class UserService {
 		Optional<User> user = repository.findById(id);
 		
 		return user.orElseThrow(() -> new ObjectNotFoundException(id));
+	}
+	
+	public User insert(User obj) {
+		return repository.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
