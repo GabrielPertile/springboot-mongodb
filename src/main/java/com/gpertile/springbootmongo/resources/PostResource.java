4aa06b444f1b1b748e62b96cac266112abeaacee
GabrielPertile <gabriel-pertile@hotmail.com>
2023-07-13 15:16:36 -0300

    Consulta com vários critérios

--- src/main/java/com/gpertile/springbootmongo/resources/PostResource.java
+++ src/main/java/com/gpertile/springbootmongo/resources/PostResource.java
@@ -1,5 +1,6 @@
 package com.gpertile.springbootmongo.resources;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,19 +20,35 @@ import com.gpertile.springbootmongo.services.PostService;
 public class PostResource {
 	@Autowired
 	private PostService service;
-	
+
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Post> findById(@PathVariable String id) {
 		Post post = service.findById(id);
-		
+
 		return ResponseEntity.ok().body(post);
 	}
-	
+
 	@GetMapping(value = "/titlesearch")
-	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "")	 String text) {
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
 		text = URL.decodeParam(text);
 		List<Post> list = service.findByTitle(text);
+
+		return ResponseEntity.ok().body(list);
+	}
+
+	@GetMapping(value = "/fullsearch")
+	public ResponseEntity<List<Post>> fullSearch(
+			@RequestParam(value = "text", defaultValue = "") String text,
+			@RequestParam(value = "minDate", defaultValue = "") String minDate,
+			@RequestParam(value = "maxDate", defaultValue = "") String maxDate
+			
+			) {
+		text = URL.decodeParam(text);
+		Date min = URL.convertDate(minDate, new Date(0L));
+		Date max = URL.convertDate(maxDate, new Date());
 		
+		List<Post> list = service.fullSearch(text, min, max);
+
 		return ResponseEntity.ok().body(list);
 	}
 }
