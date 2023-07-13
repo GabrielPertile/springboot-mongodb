92b5df0c0ae70e3c2596961c519e60ef4b346856
GabrielPertile <gabriel-pertile@hotmail.com>
2023-07-13 14:57:19 -0300

    consulta simples com @Query

--- src/main/java/com/gpertile/springbootmongo/repository/PostRepository.java
+++ src/main/java/com/gpertile/springbootmongo/repository/PostRepository.java
@@ -3,10 +3,14 @@ package com.gpertile.springbootmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import com.gpertile.springbootmongo.domain.Post;
 
 public interface PostRepository extends MongoRepository<Post, String>{
 
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> findByTitle(String text);
+	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 }
